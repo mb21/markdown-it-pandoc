021e60049c41ac392075fd04bdb75c297017fc52
mb21 <mb21@users.noreply.github.com>
2021-07-30 17:01:42 +0200

    add TypeScript definitions

---
+++ index.d.ts
@@ -0,0 +1,16 @@
+import MarkdownIt from 'markdown-it'
+
+declare const markdownItPandoc: (md: MarkdownIt, opts?: {
+  attributes:       boolean;
+  bracketed_spans:  boolean;
+  definition_lists: boolean;
+  fenced_divs:      boolean;
+  footnotes:        boolean;
+  implicit_figures: boolean;
+  grid_tables:      boolean;
+  katex:            boolean;
+  subscript:        boolean;
+  superscript:      boolean;
+}) => MarkdownIt
+
+export default markdownItPandoc
