f57fce22d5640be4d539bdd2273f11cbe12e526b
mb21 <mb21@users.noreply.github.com>
2021-08-30 19:37:54 +0200

    fix type definitions

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,17 @@
 import MarkdownIt from 'markdown-it'
 
 declare const markdownItPandoc: (md: MarkdownIt, opts?: {
-  attributes:       boolean;
-  bracketed_spans:  boolean;
-  definition_lists: boolean;
-  fenced_divs:      boolean;
-  footnotes:        boolean;
-  implicit_figures: boolean;
-  grid_tables:      boolean;
-  katex:            boolean;
-  subscript:        boolean;
-  superscript:      boolean;
-  task_lists:       boolean;
+  attributes?:       boolean;
+  bracketed_spans?:  boolean;
+  definition_lists?: boolean;
+  fenced_divs?:      boolean;
+  footnotes?:        boolean;
+  implicit_figures?: boolean;
+  grid_tables?:      boolean;
+  katex?:            boolean;
+  subscript?:        boolean;
+  superscript?:      boolean;
+  task_lists?:       boolean;
 }) => MarkdownIt
 
 export default markdownItPandoc
